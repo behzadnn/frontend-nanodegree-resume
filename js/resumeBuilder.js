5e02b4443a0849865c8a9edfb817fde91a9e70b7
behzad najafi <behzad najafi>
2016-02-05 13:59:26 +0100

    updating the resumberbuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,12 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ //$("#main").append(["behzad"])
+ //var AwesomeThoughts= " I am  Behzad and I am Awesome"
+ //FunThoughts=AwesomeThoughts.replace("Awesome","Fun")
+ // $("#main").append(FunThoughts)
+  //Here I am cahnging the variable defined in helper.js  
+  //and replacing %data% by my name.
+
+  var formattedName =HTMLheaderName.replace("%data%",name)
+  $("#header").append(formattedName)
